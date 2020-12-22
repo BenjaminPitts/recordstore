bd2bbfaf443952e78f220ef155cc01e5c510ec40
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2020-12-22 12:46:45 -0700

    set up file structure

--- server.js
+++ server.js
@@ -4,28 +4,40 @@ const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 const db = mongoose.connection
+const session = require('express-session')
 require('dotenv').config()
 //port for heroku
 const PORT = process.env.PORT || 3003
 //database connection via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI
 //mongoose connection statement
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI,
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false })
+  useFindAndModify: false
+})
 //error-success messages
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
 //middleware
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 app.use(methodOverride('_method'))
+// app.use(
+//   session({
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// )
+
 //routes
 app.get('/', (req, res)=>{
-  res.send('hello chef!')
+  res.send('Hello chef!')
 })
 
 //listening PORT
